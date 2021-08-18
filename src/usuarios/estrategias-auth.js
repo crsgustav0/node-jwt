9aca87f3ba1ca70d0bb8393a389be6f7d35ebfc3
Crs <cristian.dias@terraviva.agr.br>
2021-08-17 21:53:44 -0300

    feat: Implementação validações autenticação rotas Usuários, arquivos 'estrategias-auth.js'

---
+++ src/usuarios/estrategias-auth.js
@@ -0,0 +1,91 @@
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy;
+/*
+Importa o modelo Usuarios para utilizar,
+retornar dados a partir dos métodos próprios
+*/
+const Usuario = require('./usuarios-modelo')
+
+/*
+Importa o modelo utilizado para retorno de 
+validações genéricas
+*/
+const { InvalidArgumentError } = require('../erros')
+
+/*
+Importa a biblioteca bcrypt para validação das senhas
+*/
+const bcrypt = require('bcrypt')
+
+/*
+Função criada para centralizar validações
+referente ao usuário 
+*/
+function validarUsuario(usuario) {
+    if (!usuario) {
+        throw new InvalidArgumentError('Usuário a partir do e-mail não encontrado');
+    }
+
+}
+
+/*
+Função criada para centralizar validações
+referente ao campo senha
+*/
+async function validarSenha(senha, senhaHash) {
+    /*
+    Comparação das senhas enviadas via requisição,
+    retornando a partir da execução uma 'promisse'
+    */
+    const senhaValida = await bcrypt.compare(senha, senhaHash);
+    if (!senhaValida) {
+        throw new InvalidArgumentError('E-mail ou senha inválidos')
+    }
+}
+
+/*
+
+Método utilizado na configuração de novas estratégias de
+autenticação, passando via parâmetro o objeto que será 
+utilizado e a função responsável pela validação
+
+done - usado para o retorno a partir da autenticação 
+passando via parâmetros os campos utilizados 
+
+*/
+passport.use(
+    new LocalStrategy({
+            usernameField: 'email',
+            passwordField: 'senha',
+            /*Login sem sessões*/
+            session: false
+
+        },
+        /*
+
+        Async - Adicionado a função devido ao recebimento da resposta via 'promisse' através da função 'await'
+
+        */
+        async(email, senha, done) => {
+            try {
+                /*
+            
+                Realiza a busca de registros passando via parâmetro
+                o campo email, recibido a partir da requisição
+            
+                */
+                const usuario = await Usuario.buscaPorEmail(email);
+                /*Validações referentes aos campos 
+                chave para autenticação*/
+                validarUsuario(usuario);
+                validarSenha(senha, usuario.senhaHash);
+
+                /*Finaliza a autenticação dos dados, retornando via
+                'callback' erros não encontrados (nulos) e 
+                o usuário autenticado*/
+                done(null, usuario)
+            } catch (error) {
+                done(error);
+            }
+        })
+)
